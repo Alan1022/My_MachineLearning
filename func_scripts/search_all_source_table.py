cf30f63759c993524402baa94d46e55c508e1b97
Alan <763202606@qq.com>
2018-10-09 10:56:02 +0800

    update

--- func_scripts/search_all_source_table.py
+++ func_scripts/search_all_source_table.py
@@ -17,16 +17,19 @@ with open (hql_file,"r") as f:
     for line in f.readlines():  #readlines()则一次性读取所有的内容按行返回
         hql=line.upper()
         hql=hql.replace("  "," ")  #双空格替换为单空格
-        # print "正在查找这行代码：" + hql
+        print "正在查找这行代码：" + hql
         #http://www.runoob.com/python/python-reg-expressions.html#flags
-        m1 = re.match(r".*FROM\s([^TMP][A-Z]{1,5})\.([A-Z0-9\_]+)", hql, re.I | re.M)
-        m2 = re.match(r".*JOIN\s([^TMP][A-Z]{1,5})\.([A-Z0-9\_]+)", hql, re.I | re.M)
+        # 不区分大小写，多行匹配
+        m1 = re.search(r".*FROM\s+([A-Z\_]+)\.([A-Z0-9\_]+)", hql, re.I | re.M)
+        m2 = re.search(r".*JOIN\s+([A-Z\_]+)\.([A-Z0-9\_]+)", hql, re.I | re.M)
         if m1:
             table1=m1.group(1)+"."+m1.group(2)
             source_tables = source_tables + "," + table1
+            print(table1)
         if m2:
             table2=m2.group(1)+"."+m2.group(2)
             source_tables = source_tables + "," + table2
+
     #切分成list
     list_tables = source_tables.split(",")
     #去重
